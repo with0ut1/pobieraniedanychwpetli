de34f84e8b2559e575ab9e3d55ce00b951beeae5
4198787 <4198787@TE-GDA-D-D21E16>
2025-06-09 10:01:53 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Podaj liczbę ocen: ");
+int liczbaOcen = int.Parse(Console.ReadLine());
+
+int[] oceny = new int[liczbaOcen];
+
+for (int i = 0; i < liczbaOcen; i++)
+{
+    Console.Write($"Podaj ocenę {i + 1}: ");
+    oceny[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("\nWprowadzone oceny:");
+for (int i = 0; i < liczbaOcen; i++)
+{
+    Console.WriteLine($"Ocena {i + 1}: {oceny[i]}");
+}
